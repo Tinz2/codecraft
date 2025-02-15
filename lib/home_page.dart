fdf5c65a17d080de87dc556c9d57ea2d1af59785
Teerachai <Teerachai2546gg@gmail.com>
2025-02-15 23:01:22 +0530

    เพิ่มจากทิน กดlogoutแล้วไปหน้าสมัคร
    เพิ่มจากทิน กดlogoutแล้วไปหน้าสมัคร

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,34 +1,270 @@
 import 'package:flutter/material.dart';
 import 'auth.dart';
 import 'menu.dart';
-import 'sign_in.dart'; // เพิ่มการนำเข้า LoginPage
+import 'sign_in.dart';
 
 class HomePage extends StatelessWidget {
   static const String routeName = '/home';
   final AuthService _auth = AuthService();
+
   HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        backgroundColor: Colors.black,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.5),
+              child: Image.asset(
+                'assets/logo.png',
+                height: 39,
+                width: 39,
+              ),
+            ),
+            SizedBox(width: 8),
+            ShaderMask(
+              shaderCallback: (bounds) => LinearGradient(
+                colors: [Color(0xFF0151FF), Color(0xFF0095FF)],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ).createShader(bounds),
+              child: Text(
+                'CodeCraft',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
         actions: [
           TextButton.icon(
             onPressed: () async {
-              await _auth.signOut(context); // ส่ง context ไปยังฟังก์ชัน signOutเพื่อเรียกใช้ signout
-              Navigator.pushReplacementNamed(context, LoginPage.routeName); // เพิ่มการนำทางไปยังหน้า LoginPage
+              await _auth.signOut(context);
+              Navigator.pushReplacementNamed(context, LoginPage.routeName);
             },
             icon: Icon(Icons.person, color: Colors.white),
             label: Text('Logout', style: TextStyle(color: Colors.white)),
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          'You are logged in!',
-          style: TextStyle(fontSize: 24.0),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                      text: 'Learn Code By\n',
+                      style: TextStyle(
+                          fontSize: 39,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  TextSpan(
+                      text: 'Playing ',
+                      style: TextStyle(
+                          fontSize: 39,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  TextSpan(
+                      text: 'Games',
+                      style: TextStyle(
+                          fontSize: 39,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue)),
+                ],
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                'Learning to code is no longer a dull experience with ITINs. '
+                'We transform the learning process into an engaging and interactive experience, '
+                'making it as fun and exciting as playing a game.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 19,
+                    color: Colors.white,
+                    decoration: TextDecoration.underline),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFF0151FF), Color(0xFF0095FF)],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: SizedBox(
+                width: 250,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                  child: Text(
+                    'Play and Learn Code',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Container(
+                height: 250,
+                width: 450,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 9, 109, 54),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey, width: 2),
+                ),
+                child: Center(
+                  child: Text(
+                    'พื้นที่ว่างสำหรับใส่ เกม',
+                    style: TextStyle(color: Colors.grey[600], fontSize: 16),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xFF2E2E2E),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      style: TextStyle(fontFamily: 'monospace', fontSize: 16),
+                      children: [
+                        TextSpan(
+                            text: '1 ', style: TextStyle(color: Colors.grey)),
+                        TextSpan(
+                          text: 'display',
+                          style: TextStyle(
+                              color: Colors.lightGreenAccent, fontSize: 17),
+                        ),
+                        TextSpan(
+                            text: ': flex;\n',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 17)),
+                        TextSpan(
+                            text: '2 ', style: TextStyle(color: Colors.grey)),
+                        TextSpan(
+                          text: 'justify-content',
+                          style: TextStyle(
+                              color: Colors.lightGreenAccent, fontSize: 17),
+                        ),
+                        TextSpan(
+                            text: ': center;',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 17)),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Text(
+                        '3 ',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'monospace',
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF5A5A5A),
+                            borderRadius: BorderRadius.circular(6),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                offset: Offset(2, 2),
+                                blurRadius: 4,
+                              ),
+                            ],
+                          ),
+                          child: TextField(
+                            style: TextStyle(
+                                color: Colors.white, fontFamily: 'monospace'),
+                            decoration:
+                                InputDecoration(border: InputBorder.none),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFF0095FF), Color(0xFF0151FF)],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+                child: SizedBox(
+                  width: 150,
+                  height: 50,
+                  child: Center(
+                    child: Text(
+                      'Check Answer',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
         ),
       ),
+      backgroundColor: Colors.black,
     );
   }
 }
